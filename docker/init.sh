8a46106336fd09a5f1a29c92bf1b57c242f096f2
Organi-Group <ahmed.zaki@mkenterprise-eg.com>
2025-12-01 01:16:23 +0200

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,5 +1,15 @@
 #!bin/bash
 
+FROM frappe/bench:latest
+
+USER root
+
+RUN mkdir -p /workspace
+COPY init.sh /workspace/init.sh
+RUN chmod +x /workspace/init.sh
+
+USER frappe
+
 if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
     echo "Bench already exists, skipping init"
     cd frappe-bench

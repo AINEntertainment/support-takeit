6db37f0eb89b83104b2f99a4d20b80a9624902a8
Organi-Group <ahmed.zaki@mkenterprise-eg.com>
2025-12-01 12:59:36 +0200

    Update Dockerfile and init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,19 +1,9 @@
-#!/bin/bash
-BENCH_DIR="/home/frappe/frappe-bench"
-FRAPPE_USER_UID=1000
+#!bin/bash
 
-# Fix 1: Ensure the frappe user owns the mounted volume directories
-# This resolves the PermissionError on logs and sites files.
-echo "Setting ownership of $BENCH_DIR to user $FRAPPE_USER_UID..."
-sudo chown -R $FRAPPE_USER_UID:$FRAPPE_USER_UID $BENCH_DIR
-echo "Ownership set."
-
-# Use the 'sites' directory as the existence check, as it's created during a successful bench init.
-if [ -d "$BENCH_DIR/sites" ]; then
-    echo "Bench already exists, skipping initialization."
-    cd $BENCH_DIR
-    # Use 'exec' to replace the shell process with bench start
-    exec bench start
+if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
+    echo "Bench already exists, skipping init"
+    cd frappe-bench
+    bench start
 else
     echo "Creating new bench..."
 fi

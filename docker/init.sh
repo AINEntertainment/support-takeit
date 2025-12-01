13a2c171131340ea5340b58a22d0fda1e596f0b2
Organi-Group <ahmed.zaki@mkenterprise-eg.com>
2025-12-01 17:03:25 +0200

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,12 +1,12 @@
 #!bin/bash
 
-if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
-    echo "Bench already exists, skipping init"
-    cd frappe-bench
-    bench start
-else
-    echo "Creating new bench..."
-fi
+#if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
+#    echo "Bench already exists, skipping init"
+#    cd frappe-bench
+#    bench start
+#else
+#    echo "Creating new bench..."
+#fi
 
 #bench init --skip-redis-config-generation frappe-bench --version version-15
 

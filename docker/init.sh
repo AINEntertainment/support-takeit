799fba322f7109b41796db71a9cc6674cac4c2d4
Organi-Group <ahmed.zaki@mkenterprise-eg.com>
2025-12-01 14:13:31 +0200

    Update Docker and init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -1,6 +1,5 @@
 #!bin/bash
-chown -R frappe:frappe /home/frappe/
-chown -R frappe:frappe /workspace/
+
 
 if [ -d "/home/frappe/frappe-bench/" ]; then
     echo "Bench already exists, skipping init"
